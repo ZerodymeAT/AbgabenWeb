0950e421c1086cecb9788e35faab5e403bd9c052
Sebastian Otter-Bäck <113764844+ZerodymeAT@users.noreply.github.com>
2023-01-06 17:53:34 +0100

    update with switch

--- Aufgaben/Mathequiz/script/mathescript.js
+++ Aufgaben/Mathequiz/script/mathescript.js
@@ -1,48 +1,96 @@
 'use strict';
-let level = 0;
+let level = 4;
 let lifes = 3;
+let userAns = document.getElementById("btnUserAnswer");
 
-function game(answer) {
-    let result;
-    let useranswer = answer;
-    function getRandomInt() {
-        if (level <= 3) {
-            return Math.random() * (20 - 1);
-        } else if (level > 3 && level < 6) {
-            return Math.random() * (50 - 3);
-        } else if (level > 6) {
-            return Math.random() * (100 - 3);
-        }
+userAns.addEventListener('click', function () {
+    let user = document.getElementById("userMatheAnswer").value;
+});
+
+function game() {
+    let int1 = Math.round(getRandomInt(level));
+    let int2 = Math.round(getRandomInt(level));
+    if (int2 > int1) {
+        let tempint = int1;
+        int1 = int2;
+        int2 = tempint;
     }
 
-    function levelplus() {
-        if (level === 1 || level === 5) {
-            result = getRandomInt() + getRandomInt();
-        }
-        return result;
+    document.querySelector('#question').innerHTML = ("Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " =  ");
+    console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + switchCalc(int1, int2) + "  " + userAns);
+
+    if (userAns === switchCalc(int1, int2)) {
+        level++;
+    } else {
+        lifes--;
     }
-
-    function levelminus() {
-        if (level === 2 || level === 6) {
-            result = getRandomInt() - getRandomInt();
-        }
-        return result;
-    }
-
-    function levelmulti() {
-        if (level === 3 || level === 7) {
-            result = getRandomInt() * getRandomInt();
-        }
-        return result;
-    }
-    function leveldivision() {
-        if (level === 4 || level > 7) {
-            result = Math.round((getRandomInt() / getRandomInt())*100) / 100;
-        }
-        return result;
-    }
-
-
-
 
 }
+
+function getRandomInt() {
+    if (level <= 3) {
+        return Math.random() * (20 - 1);
+    } else if (level > 3 && level <= 6) {
+        return Math.random() * (50 - 3);
+    } else if (level > 6) {
+        return Math.random() * (100 - 3);
+    }
+}
+
+function switchCalc(int1, int2) {
+    let result;
+    // let divint1;
+    // let divint2;
+
+    switch (level) {
+        case 1: case 5:
+            result = int1 + int2;
+            break;
+        case 2: case 6:
+            result = int1 - int2;
+            break;
+        case 3: case 7:
+            result = int1 / int2;
+            break;
+        case 4: case 8: case 9:
+            result = int1 * int2;
+            break;
+    }
+    return result;
+}
+
+function switchOp(level){
+    let op;
+    switch (level){
+        case 1: case 5: op = '+'; break;
+        case 2: case 6: op = '-'; break;
+        case 3: case 7: op = '/'; break;
+        case 4: case 8: case 9: op = '*'; break;
+    }
+    return op;
+}
+
+
+
+
+
+// function operatorSelection() {
+//     let operator;
+//     if (level === 1 || level === 5) {
+//         operator = '+';
+//         return operator;
+//     } else if (level === 2 || level === 6) {
+//         operator = '-';
+//         return operator;
+//     } else if (level === 3 || level === 7) {
+//         operator = '/';
+//         return operator;
+//     } else if (level === 4 || level > 7) {
+//         operator = '*';
+//         return operator;
+//     }
+// }
+//
+// function mathResult(int1, int2) {
+//     return eval(int1 + operatorSelection(level) + int2);
+// }
