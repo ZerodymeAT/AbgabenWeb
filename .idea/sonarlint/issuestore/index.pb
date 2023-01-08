9ad0165053bc861ed929471a0df25ece5e226d01
Sebastian O-B <113764844+ZerodymeAT@users.noreply.github.com>
2023-01-08 23:42:53 +0100

    Frustlevel 3 von 5 erreich

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+M
+Aufgaben/Mathequiz/index.html,8\e\8eb137c2bc95584f3e7eb803445837e6bee79ca3
+X
+(Aufgaben/Mathequiz/script/mathescript.js,a\2\a23c9ea136bab3ed8de8647c10c8cbfb16cedcbf
--- Aufgaben/Mathequiz/script/mathescript.js
+++ Aufgaben/Mathequiz/script/mathescript.js
@@ -1,38 +1,23 @@
 'use strict';
 let level = 1;
 let lifes = 3;
-let userAns = 0;
+let userAns;
+let int1;
+let int2;
+let result;
 
-function userAnswer() {
-    userAns = document.querySelector('#userMatheAnswer');
-}
+function comparisonWithUserInput() {
+    userAnswer();
 
-// userAns.addEventListener('click', function () {
-//     let user = document.getElementById("userMatheAnswer").value;
-// });
-
-function game(userAns) {
-    let int1 = getRandomInt(level);
-    let int2 = getRandomInt(level);
-    if (int2 > int1) {
-        let tempint = int1;
-        int1 = int2;
-        int2 = tempint;
+    if (userAns === result) {
+        level++;
+        alert("Yeha");
+    } else if (userAns !== result){
+        if (lifes > 0){
+            lifes--;
+            alert("Noch " + lifes + " über");
+        } else gameOver();
     }
-    let result = switchCalc(int1, int2);
-
-    document.querySelector('#question').innerHTML = ("Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " =  ");
-    console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + result + "  " + userAnswer());
-
-
-
-    if (lifeCalculator() === 1) {
-        if (userAns === result) {
-            level++;
-            console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + result + "  " + userAnswer());
-            game(userAns, level, lifes);
-        } else lifes--;
-    } else gameOver();
 }
 
 function lifeCalculator() {
@@ -41,92 +26,81 @@ function lifeCalculator() {
     } else return 0;
 }
 
-/* Returns switchCalc result */
-function switchCalc(int1, int2) {
-    let result;
-    // let divint2;
+function userAnswer() {
+    userAns = document.getElementById('userMatheAnswer');
+    return userAns;
+}
 
+function levelbasedCalculation() {
+    int1 = getRandomInt();
+    int2 = getRandomInt();
+    if (int2 > int1) {
+        let tempint = int1;
+        int1 = int2;
+        int2 = tempint;
+    }
+    let textbaustein = "Aufgabe (" + level + "): ";
     switch (level) {
         case 1:
         case 5:
+            document.getElementById("start").innerHTML = textbaustein + int1 + " + " + int2;
             result = int1 + int2;
+            console.log("Level: " + level + " Aufgabe: " + int1 + " + " + int2 + " = " + result);
             break;
         case 2:
         case 6:
+            document.getElementById("start").innerHTML = textbaustein + int1 + " - " + int2;
             result = int1 - int2;
+            console.log("Level: " + level + " Aufgabe: " + int1 + " - " + int2 + " = " + result);
             break;
         case 3:
         case 7:
+            document.getElementById("start").innerHTML = textbaustein + int1 + " / " + int2;
             result = int1 / int2;
+            console.log("Level: " + level + " Aufgabe: " + int1 + " / " + int2 + " = " + result);
             break;
         case 4:
         case 8:
-        case 9:
+            document.getElementById("start").innerHTML = textbaustein + int1 + " * " + int2;
             result = int1 * int2;
+            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " = " + result);
+            break
+        case 9:
+            document.getElementById("start").innerHTML = textbaustein + int1 + " * " + int2 + " + " + (int2 * 3);
+            result = int1 * int2 + (int2 * 3);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " + " + (int2 * 3) + " = " + result);
             break;
     }
     return result;
 }
 
-/* Returns the operator [op] for the calculation */
-function switchOp(level) {
-    let op;
-    switch (level) {
-        case 1:
-        case 5:
-            op = '+';
-            break;
-        case 2:
-        case 6:
-            op = '-';
-            break;
-        case 3:
-        case 7:
-            op = '/';
-            break;
-        case 4:
-        case 8:
-        case 9:
-            op = '*';
-            break;
-    }
-    return op;
-}
-
 function getRandomInt() {
     if (level <= 3) {
-        return Math.floor(Math.random() * (20 - 2));
+        return Math.floor(Math.random() * 20) + 2;
     } else if (level > 3 && level <= 6) {
-        return Math.floor(Math.random() * (50 - 5));
+        return Math.floor(Math.random() * 50) + 5;
     } else if (level > 6) {
-        return Math.floor(Math.random() * (100 - 10));
+        return Math.floor(Math.random() * 100) + 10;
     }
 }
+
 function newGame() {
-    window.location.reload()
+    window.location.reload();
 }
+
+function letTheShowBegin() {
+    if (level > 0) {
+        levelbasedCalculation();
+        comparisonWithUserInput();
+    } else {
+        document.getElementById('start').innerHTML = `Spiel starten`;
+    }
+}
+
 function gameOver() {
     console.log("Game Over")
 }
 
-
-// function operatorSelection() {
-//     let operator;
-//     if (level === 1 || level === 5) {
-//         operator = '+';
-//         return operator;
-//     } else if (level === 2 || level === 6) {
-//         operator = '-';
-//         return operator;
-//     } else if (level === 3 || level === 7) {
-//         operator = '/';
-//         return operator;
-//     } else if (level === 4 || level > 7) {
-//         operator = '*';
-//         return operator;
-//     }
-// }
-//
-// function mathResult(int1, int2) {
-//     return eval(int1 + operatorSelection(level) + int2);
-// }
+function start() {
+    return level = 1;
+}
