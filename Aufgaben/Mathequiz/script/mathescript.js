c59527b6541192c71368b9a560195a9e450e8edd
Sebastian Otter-Bäck <113764844+ZerodymeAT@users.noreply.github.com>
2023-01-08 20:24:35 +0100

    change to pc

--- Aufgaben/Mathequiz/script/mathescript.js
+++ Aufgaben/Mathequiz/script/mathescript.js
@@ -1,77 +1,113 @@
 'use strict';
-let level = 4;
+let level = 1;
 let lifes = 3;
-let userAns = document.getElementById("btnUserAnswer");
+let userAns = 0;
 
-userAns.addEventListener('click', function () {
-    let user = document.getElementById("userMatheAnswer").value;
-});
+function userAnswer() {
+    userAns = document.querySelector('#userMatheAnswer');
+}
 
-function game() {
-    let int1 = Math.round(getRandomInt(level));
-    let int2 = Math.round(getRandomInt(level));
+// userAns.addEventListener('click', function () {
+//     let user = document.getElementById("userMatheAnswer").value;
+// });
+
+function game(userAns) {
+    let int1 = getRandomInt(level);
+    let int2 = getRandomInt(level);
     if (int2 > int1) {
         let tempint = int1;
         int1 = int2;
         int2 = tempint;
     }
+    let result = switchCalc(int1, int2);
 
     document.querySelector('#question').innerHTML = ("Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " =  ");
-    console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + switchCalc(int1, int2) + "  " + userAns);
+    console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + result + "  " + userAnswer());
 
-    if (userAns === switchCalc(int1, int2)) {
-        level++;
-    } else {
-        lifes--;
-    }
 
+
+    if (lifeCalculator() === 1) {
+        if (userAns === result) {
+            level++;
+            console.log("Level: " + level + " Aufgabe: " + int1 + " " + switchOp(level) + " " + int2 + " = " + result + "  " + userAnswer());
+            game(userAns, level, lifes);
+        } else lifes--;
+    } else gameOver();
 }
 
-function getRandomInt() {
-    if (level <= 3) {
-        return Math.random() * (20 - 1);
-    } else if (level > 3 && level <= 6) {
-        return Math.random() * (50 - 3);
-    } else if (level > 6) {
-        return Math.random() * (100 - 3);
-    }
+function lifeCalculator() {
+    if (lifes > 0) {
+        return 1;
+    } else return 0;
 }
 
+/* Returns switchCalc result */
 function switchCalc(int1, int2) {
     let result;
-    // let divint1;
     // let divint2;
 
     switch (level) {
-        case 1: case 5:
+        case 1:
+        case 5:
             result = int1 + int2;
             break;
-        case 2: case 6:
+        case 2:
+        case 6:
             result = int1 - int2;
             break;
-        case 3: case 7:
+        case 3:
+        case 7:
             result = int1 / int2;
             break;
-        case 4: case 8: case 9:
+        case 4:
+        case 8:
+        case 9:
             result = int1 * int2;
             break;
     }
     return result;
 }
 
-function switchOp(level){
+/* Returns the operator [op] for the calculation */
+function switchOp(level) {
     let op;
-    switch (level){
-        case 1: case 5: op = '+'; break;
-        case 2: case 6: op = '-'; break;
-        case 3: case 7: op = '/'; break;
-        case 4: case 8: case 9: op = '*'; break;
+    switch (level) {
+        case 1:
+        case 5:
+            op = '+';
+            break;
+        case 2:
+        case 6:
+            op = '-';
+            break;
+        case 3:
+        case 7:
+            op = '/';
+            break;
+        case 4:
+        case 8:
+        case 9:
+            op = '*';
+            break;
     }
     return op;
 }
 
-
-
+function getRandomInt() {
+    if (level <= 3) {
+        return Math.floor(Math.random() * (20 - 2));
+    } else if (level > 3 && level <= 6) {
+        return Math.floor(Math.random() * (50 - 5));
+    } else if (level > 6) {
+        return Math.floor(Math.random() * (100 - 10));
+    }
+}
+function newGame() {
+    window.location.reload()
+}
+function gameOver() {
+    console.log("Game Over")
+}
 
 
 // function operatorSelection() {
