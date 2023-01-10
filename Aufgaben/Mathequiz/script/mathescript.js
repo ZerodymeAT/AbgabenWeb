0aa8d1268ab9e0c22472914088fe71e0c3a15da2
Sebastian Otter-Bäck <113764844+ZerodymeAT@users.noreply.github.com>
2023-01-10 21:49:34 +0100

    funkt

--- Aufgaben/Mathequiz/script/mathescript.js
+++ Aufgaben/Mathequiz/script/mathescript.js
@@ -8,10 +8,9 @@ let result;
 
 function comparisonWithUserInput() {
     userAns = userAnswer();
-    document.getElementById('userMatheAnswer').innerHTML = "";
+    document.getElementById('userMatheAnswer').value = "";
     if (userAns === result) {
         level++;
-
         levelbasedCalculation();
     } else gameOver();
 }
@@ -33,37 +32,47 @@ function levelbasedCalculation() {
         case 1:
         case 5:
             document.getElementById("start").innerHTML = textbaustein + int1 + " + " + int2;
+            document.getElementById("Level"+level).style.color = 'red';
             result = int1 + int2;
-            console.log("Level: " + level + " Aufgabe: " + int1 + " + " + int2 + " = " + result);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " + " + int2 + " = " + result + " Lifes: " + lifes);
             break;
         case 2:
         case 6:
             document.getElementById("start").innerHTML = textbaustein + int1 + " - " + int2;
             result = int1 - int2;
-            console.log("Level: " + level + " Aufgabe: " + int1 + " - " + int2 + " = " + result);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " - " + int2 + " = " + result +" Lifes: " + lifes);
             break;
         case 3:
         case 7:
             document.getElementById("start").innerHTML = textbaustein + int1 + " / " + int2;
-            result = int1 / int2;
-            console.log("Level: " + level + " Aufgabe: " + int1 + " / " + int2 + " = " + result);
+            result = Math.floor(int1 / int2);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " / " + int2 + " = " + result +" Lifes: " + lifes);
             break;
         case 4:
         case 8:
             document.getElementById("start").innerHTML = textbaustein + int1 + " * " + int2;
             result = int1 * int2;
-            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " = " + result);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " = " + result + "Lifes: " + lifes);
             break
         case 9:
             document.getElementById("start").innerHTML = textbaustein + int1 + " * " + int2 + " + " + (int2 * 3);
             result = int1 * int2 + (int2 * 3);
-            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " + " + (int2 * 3) + " = " + result);
+            console.log("Level: " + level + " Aufgabe: " + int1 + " * " + int2 + " + " + (int2 * 3) + " = " + result + " Lifes: " + lifes);
             break;
     }
 
-    return result;
+    // return Math.floor(result);
 }
 
+function styling(){
+    let passed;
+
+    if (level )
+    document.getElementById("Level"+level).style.color = 'red';
+    document.getElementById("Level"+level).style.borderRadius = '50%';
+    document.getElementById("Level"+level).image = ''
+
+}
 function getRandomInt() {
     if (level <= 3) {
         return Math.floor(Math.random() * 20) + 2;
@@ -90,10 +99,15 @@ function letTheShowBegin() {
 function gameOver() {
     if (lifes > 0){
         lifes--;
-    } else alert("Game Over")
+    } else {
+        alert("Game Over du Noob");
+        newGame();
+    }
 }
 
 function start() {
-    level = 1;
-    levelbasedCalculation();
+    if (level === 0) {
+        level = 1;
+        letTheShowBegin();
+    } else letTheShowBegin();
 }
