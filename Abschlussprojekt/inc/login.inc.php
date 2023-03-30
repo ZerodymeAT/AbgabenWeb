31b20a63ff95bd1c935ec71151c391c5b4891109
Sebastian Otter-Bäck <113764844+ZerodymeAT@users.noreply.github.com>
2023-03-30 19:40:36 +0200

    works bitch

--- Abschlussprojekt/inc/login.inc.php
+++ Abschlussprojekt/inc/login.inc.php
@@ -1,8 +1,18 @@
 <?php
 
 try {
-    $pdo = new PDO('codersbay-nation.c9mh1sytq7cn.eu-central-1.rds.amazonaws.com:3306', 'root', 'codersbayroot');
+    $pdo = new PDO('mysql:host=codersbay-nation.c9mh1sytq7cn.eu-central-1.rds.amazonaws.com:3306', 'root', 'codersbayroot');
 } catch (PDOException $e) {
+
     print "Error!: " . $e->getMessage() . "<br/>";
-    die();#
+    die();
+}
+
+
+$mysqli = new mysqli("codersbay-nation.c9mh1sytq7cn.eu-central-1.rds.amazonaws.com:3306","root","codersbayroot","otterbaeck");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
 }
